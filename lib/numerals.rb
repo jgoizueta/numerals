14ffd2a1acd95f7d85068e037b646be549ac93a4
Javier Goizueta <jgoizueta@gmail.com>
2014-10-28 22:42:31 +0100

    Missing from previous commit: main required file

--- lib/numerals.rb
+++ lib/numerals.rb
@@ -1,5 +1,13 @@
-require "numerals/version"
 require 'modalsupport'
+
+require "numerals/version"
 require 'numerals/numeral'
+require 'numerals/rounding'
+require 'numerals/conversions/float'
+require 'numerals/conversions/integer'
+require 'numerals/conversions/rational'
+require 'numerals/conversions/bigdecimal'
+require 'numerals/conversions/flt'
+
 require 'numerals/rtnlzr'
 require 'numerals/fmt'

59583a12e8ab7dfd07c3d9170466edeff5ba8d5e
Javier Goizueta <jgoizueta@gmail.com>
2015-03-07 23:01:37 +0100

    Remove old Conversions interface

--- lib/numerals/conversions.rb
+++ lib/numerals/conversions.rb
@@ -11,77 +11,7 @@ module Numerals::Conversions
       self[number.class].order_of_magnitude(number, options)
     end
 
-    # Convert a numeric value to a Numeral.
-    #
-    #  number_to_numeral x, mode, rounding
-    #
-    # There's two ways of defining the conversion mode:
-    #
-    # 1. fixed or free:
-    #
-    # * :fixed means: adjust the number precision to the output rounding
-    # * :free means: forget about the rounding (except for the base);
-    #   preserve the input number precision
-    #
-    # But :free cheats: if rounding is not exact it really honors it by
-    # converting the number to an exact numeral an then rounding it.
-    #
-    # 2. exact or approximate:
-    #
-    # * :exact means: consider the number an exact quantity
-    # * :approximate means: consider the number approximate;
-    #   show only non-spurious digits.
-    #
-    def number_to_numeral(number, *args)
-      # TODO: default mode value:
-      # if no rounding/rounding options are passed except for the base,
-      # the default mode should be :free; otherwise, the default mode should be
-      # :fixed
-      #
-      # number_to_numeral(x, precision: 3)
-      # == number_to_numeral(x, :fixed, Rounding[precision: 3])
-      # number_to_numeral(x, precision: 3, base: 2)
-      # == number_to_numeral(x, :fixed, Rounding[precision: 3, base: 2])
-      # number_to_numeral(x, :exact, base: 2)
-      # === number_to_numeral(x, :free, Rounding[:exact, base: 2])
-      mode = extract_mode_from_args!(args)
-      rounding = Rounding[*args]
-      # mode ||= rounding.exact? ? :free : :fixed
-      mode ||= :approximate
-      if [:fixed, :free].include?(mode)
-        mode = (mode == :fixed) == (rounding.exact?) ? :exact : :approximate
-      end
-      self[number.class].number_to_numeral(number, mode, rounding)
-    end
-
-    # Convert a Numeral to a numeric value of class type:
-    #
-    #   numeral_to_number numeral, type, mode, rounding
-    #
-    # The conversion mode can be:
-    #
-    # * :fixed: interprets the input as an exact quantity and represents it
-    #   with the destination type/context precision.
-    # * :free: interprets the input as an approximate quantity with given
-    #   precision (trailing zeros being considered); the destination
-    #   type precision is ignored when possible and generates number with
-    #   equivalent (no less) precision to that of the input.
-    #   The destination rounding mode is not used to round the number
-    #   but taken into consideration so that if the result is converted back
-    #   with that rounding mode to the input precision and base, the same input
-    #   is obtained.
-    #   The :free mode is meaningless for destination types with fixed
-    #   precision such as Float.
-    def numeral_to_number(numeral, type, *args)
-      # TODO: either add :short mode or some other way to trigger that option;
-      # In the :short case, which is a variant of :free, the output doesn't have
-      # precision equivalent to the input, but just the least possible
-      # precision value that can produce back the input as described above.
-      mode = extract_mode_from_args!(args) || :fixed
-      self[type].numeral_to_number(numeral, mode, *args)
-    end
-
-    # Convert Numeral to Number, new interface
+    # Convert Numeral to Number
     #
     #   read numeral, options={}
     #
@@ -108,7 +38,7 @@ module Numerals::Conversions
       self[selector].read(numeral, exact_input, approximate_simplified)
     end
 
-    # Convert Number to Numeral, new interface
+    # Convert Number to Numeral
     #
     #   write number, options={}
     #
